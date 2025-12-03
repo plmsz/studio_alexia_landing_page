1d12edcabd9f17444ab1985006b3268270b00065
Paloma Souza <paloma.souza@ambevtech.com.br>
2025-12-03 19:33:28 -0300

    fix: server

--- studio-alexia/vite.config.ts
+++ studio-alexia/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://json-server-l09c.onrender.com/',
+        target: 'https://json-server-l09c.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },

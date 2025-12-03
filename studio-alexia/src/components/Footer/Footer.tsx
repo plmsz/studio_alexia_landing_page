ad9e0ea5b8d02b5c914f7ff8e656cb968b0dad6e
Paloma Souza <paloma.souza@ambevtech.com.br>
2025-12-03 19:41:25 -0300

    fix: correct icon imports for production build

--- studio-alexia/src/components/Footer/Footer.tsx
+++ studio-alexia/src/components/Footer/Footer.tsx
@@ -1,9 +1,10 @@
 import styles from './Footer.module.css';
+import logo from '../../assets/img/logo.png';
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <img src="/src/assets/img/logo.png" alt="Logo Studio Alexia Menezes" />
+      <img src={logo} alt="Logo Studio Alexia Menezes" />
       <p>© 2025 - Todos os direitos reservados</p>
     </footer>
   );

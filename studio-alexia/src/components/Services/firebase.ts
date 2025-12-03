a64dd8d8575af0c64c2684a78669db775ed85818
Paloma Souza <paloma.souza@ambevtech.com.br>
2025-12-03 18:26:26 -0300

    fix: firebase

--- studio-alexia/src/components/Services/firebase.ts
+++ studio-alexia/src/components/Services/firebase.ts
@@ -1,15 +1,17 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBmQ206zKQECoFl8qEOhHqYx-T2hg-kIso",
-  authDomain: "studio-alexa.firebaseapp.com",
-  projectId: "studio-alexa",
-  storageBucket: "studio-alexa.firebasestorage.app",
-  messagingSenderId: "796848375112",
-  appId: "1:796848375112:web:b461dde474d0c7c08b9f7d",
-  measurementId: "G-FLBHWLBZ8E"
+  apiKey: 'AIzaSyBmQ206zKQECoFl8qEOhHqYx-T2hg-kIso',
+  authDomain: 'studio-alexa.firebaseapp.com',
+  projectId: 'studio-alexa',
+  storageBucket: 'studio-alexa.firebasestorage.app',
+  messagingSenderId: '796848375112',
+  appId: '1:796848375112:web:b461dde474d0c7c08b9f7d',
+  measurementId: 'G-FLBHWLBZ8E',
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export { auth, GoogleAuthProvider, signInWithPopup };
